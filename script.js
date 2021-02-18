2a75a6bc703b92862ec99b4558011102bd6ae509
Claudio Boldorini Junior <cbjunior@coderp.pmrp.com.br>
2021-02-18 10:40:29 -0300

    finalizando jogo com limite do corpo da cobrinha

--- script.js
+++ script.js
@@ -34,6 +34,15 @@ function iniciarJogo(){
     if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+    for(i=1;i<snake.length;i++){
+        if (snake[i].x == snake[0].x && snake[i].y == snake[0].y){
+            clearInterval(jogo);
+            alert("Fim de Jogo :(!");
+            break;
+        }
+    }
+
     criarBG();
     criarCobrinha();
     drawFood();
